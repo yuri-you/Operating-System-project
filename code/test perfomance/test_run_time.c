73c073b9cc83292b09b02fcf6db5bb20d19f3fa7
yuri-you <yuri-you@sjtu.edu.cn>
2021-06-06 01:19:54 +0800

    final

---
+++ code/test perfomance/test_run_time.c
@@ -0,0 +1,63 @@
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+#include <unistd.h>
+#include <sched.h>
+#include <time.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/time.h> 
+#include <sched.h> 
+int main() {
+    int time1;
+    int num_pro;
+    int k;
+    int policy;
+    int success;
+    int h,i,j;
+    int s=1;
+    int flag=0;
+    int whole;
+    pid_t pid;
+    time_t t1,t2;
+    struct sched_param param;
+    param.sched_priority=0;
+    printf("The number of cycles in each process=");
+    scanf("%d",&time1);
+    printf("The number of the processes=");
+    scanf("%d",&num_pro);
+    printf("The policy you want to change into(0-NORMAL,1-FIFO,2-RR,6-WRR,7-PRR):");
+    scanf("%d",&policy);
+    if(policy!=6){
+        printf("The priority=");
+        scanf("%d",&param.sched_priority);
+    }
+    time(&t1);
+    for(k=1;k<num_pro;++k){
+        pid=fork();
+        if(pid!=0){//parent
+            success=sched_setscheduler(pid,policy,&param);
+            break;
+        }
+        else{
+            flag=1;
+        }
+    }
+    for(h=0;h<time1;++h){
+    for(i=0;i<time1;++i){
+        for(j=0;j<time1;++j){
+            s*=6;
+            s/=6;
+        }
+    }
+    }
+    wait(NULL);
+    if(flag==0){
+        time(&t2);
+        printf("%ld\n",t2-t1);
+    }
+return 0;
+}
